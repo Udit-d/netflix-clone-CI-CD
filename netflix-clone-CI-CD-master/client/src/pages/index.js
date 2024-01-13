2057fee254df64c0cac342a29b38904e5d901523
Udit Devadiga <94949574+Udit-d@users.noreply.github.com>
2024-01-14 00:27:54 +0530

    Delete netflix-clone-CI-CD-master directory

--- netflix-clone-CI-CD-master/client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { Home } from './Home/Home';
-export { Search } from './Search/Search';
-export { MediaPage } from './MediaPage/MediaPage';
-export { Watchlist } from './Watchlist/Watchlist';
--- netflix-clone-CI-CD-master/client/src/react icons/index.js
+++
@@ -1,4 +0,0 @@
-export { HomeIcon } from './HomeIcon';
-export { LibraryIcon } from './LibraryIcon';
-export { SearchIcon } from './SearchIcon';
-export { TickIcon } from './TickIcon';
